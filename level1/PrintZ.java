8086a48539e569f3bfe50b356dfe37fea9bf294e
Aayush <aayush.para7@gmail.com>
2021-08-16 13:53:25 +0530

    add printZ.java

---
+++ level1/PrintZ.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+
+public class PrintZ {
+
+  public static void main(String[] args) {
+    System.out.println("*****");
+    System.out.println("   * ");
+    System.out.println("  *  ");
+    System.out.println(" *   ");
+    System.out.println("*****");
+
+  }
+}
