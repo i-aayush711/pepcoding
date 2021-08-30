e0fab80e7bd7f7d4221f9a9af6c13313b3b1c09c
Aayush <aayush.para7@gmail.com>
2021-08-30 13:50:07 +0530

    Add 4 more func

---
+++ level1/Functions and Arrays/AnyBaseSum.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class AnyBaseSum {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int b = scn.nextInt();
+        int n1 = scn.nextInt();
+        int n2 = scn.nextInt();
+
+        int d = getSum(b, n1, n2);
+        System.out.println(d);
+    }
+
+    public static int getSum(int b, int n1, int n2) {
+        // write ur code here
+        int deci_n1 = anyToDeci(b, n1);
+        int deci_n2 = anyToDeci(b, n2);
+        int sum = deci_n1 + deci_n2;
+        int ans = deciToAny(b, sum);
+        return ans;
+    }
+
+    public static int anyToDeci(int b, int n) {
+        int temp = n;
+        int place = 1;
+        int ans = 0;
+        while (n > 0) {
+            temp = n % 10;
+            ans += temp * place;
+            place *= b;
+            n /= 10;
+        }
+        return ans;
+    }
+
+    public static int deciToAny(int b, int n) {
+        int temp = n;
+        int place = 1;
+        int ans = 0;
+        while (n > 0) {
+            temp = n % b;
+            ans += temp * place;
+            place *= 10;
+            n /= b;
+        }
+        return ans;
+    }
+}
---
+++ level1/Functions and Arrays/Span.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Span{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int [n];
+    for(int i=0;i<arr.length;i++){
+        arr[i] = scn.nextInt();
+    }
+    int min = Integer.MAX_VALUE;
+    int max= Integer.MIN_VALUE;
+    int span=0;
+    for(int i=0;i<arr.length;i++){
+        if(arr[i]<min)
+            min = arr[i];
+        if(arr[i]>max)
+            max = arr[i];
+    }
+    span = max-min;
+    System.out.println(span);
+ }
+
+}
